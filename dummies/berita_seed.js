20fa3d9ea44e27b79b1599b47de6db5c72c1c1ef
adellaaye <adellasuryaananda01@gmail.com>
2018-09-21 17:22:57 +0700

    high official

--- dummies/berita_seed.js
+++ dummies/berita_seed.js
@@ -20,4 +20,26 @@ module.exports = [
         create_at: new Date().toISOString(),
         modify_at: new Date().toISOString()
     },
+    {
+        title: "telkomtelstra promosikan Ahli IT indonesia ke kancah dunia",
+        images_url: "http://asset-a.grid.id/crop/0x0:0x0/700x465/photo/2018/09/19/3721344013.jpg",
+        content: "Liputan6.com, Melbourne - TelkomTelstra kini menjadi perusahaan manajemen aplikasi dan layanan jaringan yang mulai berkembang di Indonesia. Perusahaan ini merupakan patungan dari PT Telkom Indonesia (Persero) dengan perusahaan asal Australia, Telstra CorporationSelain jumlah konsumennya yang terus meningkat setiap tahunnya, di usianya yang menginjak 3 tahun ini, Sumber Daya Manusia (SDM) milik TelkomTelstra mulai berbicara di kancah internasionalPresiden Direktur TelkomTelstra Erik Meijer di Autralia, tengah semangat mempromosikan kemampuan para enginer ahli dari Indonesia, terutama para pegawainya Di Melbourne, Australia sendiri saat ini tengah diadakan Telstra Vantage yang menghadirkan para consumer dari berbagai negara.Di sini saya bicara keahlian Indonesia. Karena dunia luar tidak pernah menganggap Indonesia pusat IT skill. Indonesia biasanya ekspor tenaga kerja low skill. Jadi ini kita buka, kata Erik di Melbourne, Rabu (19/9/2018). Dari 180 karyawan yang dimiliki TelkomTelstra setidaknya ada 30 orang yang kini dipercaya Telstra Corporation dan beberapa mitranya di negara lain untuk terlibat dalam proyek yang dikerjakan.",
+        create_at: new Date().toISOString(),
+        modify_at: new Date().toISOString()
+    },
+    {
+        title: "saat peran manusia digantikan oleh 'Robot' di masa depan ",
+        images_url: "https://cdn.idntimes.com/content-images/post/20180205/sputniknewscom-81be2750d2403ea556dd2f414bd4173f.jpg",
+        content: 'Liputan6.com, Denpasar Perkembangan teknologi yang begitu pesat diprediksi oleh sebagian orang akan menggantikan tenaga manusia di perusahaan. Ditambah arus perubahan ekonomi yang begitu pesat, teknologi diyakini akan membuat perusahaan melakukan PHK (Pemutusan Hubungan Kerja) massal. Benarkah kekhawatiran itu? Pada pertemuan The 10th Indonesia Human Resource Summit (IHRS) 2018 hal itu terungkap. Chairman IHRS 2018, Shauqi Gombang Aleyandra mengakui ada beberapa sektor pekerjaan dalam industri yang tergantikan kecanggihan teknologi. Khususnya robot. Hanya saja, kata dia, dari hasil penelitian hingga satu dekade ke depan, paling banyak hanya sekitar 15 persen saja teknologi dapat menggantikan peran manusia."Berdasarkan hasil penelitian dari McKinsey Global Institute menyatakan hanya sekitar 5 persen dari total pekerjaan yang ada saat ini yang dapat diotomasikan secara penuh. Bahkan, hingga sepuluh tahun ke depan itu maksimal hanya 15 persen saja," papar Aleyandra, Senin (17/9/2018)',
+        create_at: new Date().toISOString(),
+        modify_at: new Date().toISOString()
+    },
+    {
+        title: "5 jenis fobia Aneh manusia dengan teknologi",
+        images_url: "http://www.loop.co.id/uploads/article/media_upload/18226/cover.jpg",
+        content: "Liputan6.com, Jakarta - Fobia  merupakan situasi atau kebiasaan di mana manusia merasakan rasa takut yang berlebihan terhadap sesuatu, apa pun itu. Fobia juga terjadi dengan hal-hal berbau teknologi. ",
+        create_at: new Date().toISOString(),
+        modify_at: new Date().toISOString()
+    }
+    
 ]
--- routes/high_official.js
+++ routes/high_official.js
@@ -15,4 +15,25 @@ router.get('/:id', (req, res, next) => {
     })
 })
 
+router.delete('/:id', (req, res, next) => {
+    queries.deletehigh_official(req.params.id).then(result => {
+        res.status(200).json(result)
+    })
+})
+
+router.post('/', (req, res, next) => {
+    queries.posthigh_official(req.body).then(result => {
+        res.status(200).json(result)
+    })
+})
+
+router.put('/:id', (req, res, next) => {
+    queries.updatehigh_official(req.params.id, req.body).then(result => {
+        res.status(200).json(result)
+    })
+})
+
+
+
+
 module.exports = router
