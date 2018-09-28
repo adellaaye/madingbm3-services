f5db4e7ed2d782e07dc77d32428af2e549aecb7c
adellaaye <adellasuryaananda01@gmail.com>
2018-09-28 15:03:51 +0700

    DB connection

--- db/knex.js
+++ db/knex.js
@@ -1,6 +1,6 @@
 const environment = process.env.NODE_ENV || 'development';
 const config = require('../knexfile');
-const environmentConfig = config[environment]; 
+const environmentConfig = config.development; 
 const knex = require('knex');
 const connection = knex(environmentConfig);
 
--- knexfile.js
+++ knexfile.js
@@ -5,6 +5,10 @@ module.exports = {
   development: {
     client: 'pg',
     connection: 'postgres://postgres:root@localhost/madingbm3'
-  }
+  },
+    production: {
+      client: 'pg',
+      connection: process.env.DATABASE_URL
+    }
   
 };
